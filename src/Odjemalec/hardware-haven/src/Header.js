3f7be614c13fc02b9c2cf38e7dc9a698f1e0bc48
AlenSkorjanc <alen.skorjanc@student.um.si>
2024-05-29 22:33:11 +0200

    header in its own file

---
+++ src/Odjemalec/hardware-haven/src/Header.js
@@ -0,0 +1,70 @@
+import { useState } from 'react';
+import './App.css';
+
+function Header() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
+  return (
+    <div className="App">
+      <header>
+        <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+          <div className="flex lg:flex-1">
+            <a href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">Hardware Haven</span>
+              <img className="h-14 w-auto" src="logo192.png" alt="Icon" />
+            </a>
+          </div>
+          <div className="flex lg:hidden">
+            <button type="button" className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-white" onClick={() => setMobileMenuOpen(true)}>
+              <span className="sr-only">Open main menu</span>
+              <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+              </svg>
+            </button>
+          </div>
+          <div className="hidden lg:flex lg:gap-x-12">
+            <a href="#" className="text-sm font-semibold leading-6 text-white">Features</a>
+            <a href="#" className="text-sm font-semibold leading-6 text-white">Marketplace</a>
+            <a href="#" className="text-sm font-semibold leading-6 text-white">Company</a>
+          </div>
+          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+            <a href="#" className="text-sm font-semibold leading-6 text-white">Log in <span aria-hidden="true">&rarr;</span></a>
+          </div>
+        </nav>
+        {mobileMenuOpen &&
+          <div className="lg:hidden" role="dialog" aria-modal="true">
+            <div className="fixed inset-0 z-10"></div>
+            <div className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-theme px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white/20">
+              <div className="flex items-center justify-between">
+                <a href="#" className="-m-1.5 p-1.5">
+                  <span className="sr-only">Hardware Haven</span>
+                  <img className="h-14 w-auto" src="logo192.png" alt="Icon" />
+                </a>
+                <button type="button" className="-m-2.5 rounded-md p-2.5 text-white" onClick={() => setMobileMenuOpen(false)}>
+                  <span className="sr-only">Close menu</span>
+                  <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                  </svg>
+                </button>
+              </div>
+              <div className="mt-6 flow-root">
+                <div className="-my-6 divide-y divide-white/20">
+                  <div className="space-y-2 py-6">
+                    <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-white/10">Features</a>
+                    <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-white/10">Marketplace</a>
+                    <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-white/10">Company</a>
+                  </div>
+                  <div className="py-6">
+                    <a href="#" className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-white hover:bg-white/10">Log in</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        }
+      </header>
+    </div>
+  );
+}
+
+export default Header;
