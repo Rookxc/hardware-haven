da42ea7fd63ba1ecb8d1e44001bbbeed11ad0f85
Rok Šekoranja <rok.sekoranja@tridenstechnology.com>
2024-06-01 20:52:55 +0200

    Fixed thank you screen & added confeti hehe

--- src/Odjemalec/hardware-haven/src/shop/ThankYou.js
+++ src/Odjemalec/hardware-haven/src/shop/ThankYou.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Confetti from 'react-confetti';
 
 function ThankYou() {
+  const [confettiVisible, setConfettiVisible] = useState(false);
+
+  useEffect(() => {
+    // Set confetti to be visible after a delay
+    const timeout = setTimeout(() => {
+      setConfettiVisible(true);
+    }, 500);
+
+    // Clean up timeout on component unmount
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
-    <div className="centered-container">
-      <h1 className="text-3xl font-bold mb-4">Thank You!</h1>
-      <p>Your purchase was successful. We appreciate your business.</p>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="container bg-white shadow-md rounded px-8 pt-6 pb-8">
+        <h1 className="text-3xl font-bold mb-8 text-center">Thank You!</h1>
+        <p className="text-lg text-center">Your purchase was successful.</p>
+        <p className="text-lg text-center">We appreciate your business.</p>
+      </div>
+      {confettiVisible && <Confetti />}
     </div>
   );
 }
